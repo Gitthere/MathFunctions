63f50ecc1c4bd38189d80a74416f1fa72540dfda
Gitthere <ichi4g@gmail.com>
2014-06-10 11:35:19 -1000

    basic math functions completed. still unsure how will get input of actual number sin

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+function add() {
+  var firstAdd = userinput;
+  var secondAdd = userinput;
+  var sum = firstAdd + secondAdd;
+  console.log(sum);
+}
+
+function subtract() {
+  var firstSubtract = userinput;
+  var secondSubtract = userinput;
+  var difference = firstSubtract - secondSubtract;
+  console.log(difference);
+}
+
+function multiply() {
+  var firstMult = userinput;
+  var secondMult = userinput;
+  var product = firstMult * secondMult;
+  console.log(product);
+}
+
+function divide() {
+  var firstDivi = userinput;
+  var secondDivi = userinput;
+  var quotient = firstDivi/secondDivi;
+}
+
+function power() {
+  var base = userinput;
+  var exponent = userinput;
+  var powerAns = Math.pow(base, exponent);
+}
+
+
+
 console.log('test');
