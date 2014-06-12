57e4a778235d7dedb7181d883cc8175db687a46d
Gitthere <ichi4g@gmail.com>
2014-06-12 10:19:47 -1000

    step 10-2 fail

--- spec/mathfunctionsspec.js
+++ spec/mathfunctionsspec.js
@@ -51,4 +51,10 @@ describe("MathFunctions", function () {
     expect(result).toBe(3);
   });
 
+  it ("should call functions and add numbers", functions() {
+    result = add22;
+    expect(result).toBe(6);
+  });
+
+
 });
