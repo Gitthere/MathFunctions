caad42745992934aa3345ce1491a247dc5183fe0
Gitthere <ichi4g@gmail.com>
2014-06-11 18:26:02 -1000

    working on step 10-10 need to confirm calculation is correct

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 (function() {
 
-
   function add(a1, a2) {
     return a1 + a2;
     console.log(add);
@@ -25,55 +24,39 @@
     return Math.pow(p1, p2);
     console.log(power);
   }
-  //NONE OF FUNCTIONS LOGGING RESULT IN CONSOLE
-  //ONLY 'TEST' APPEARS WHEN RUN NODE APP.JS
-  //WHY?????
 
   //STEP 6
   function test_add_1() {
-    var test_add_1 = add(5,4);
-    console.log(test_add_1);
+    return add(5,4);
+    // 1  
   }
-
+  
   //STEP 7
   function test_add_2() {
-    var ta1 = 5;
-    var ta2 = 4;
     var ta3 = 3;
-    var addFunc = add(5,4);
-    return ta3 + addFunc;
-    console.log(ta3 + addFunc);
+    return add(5,4) + ta3;
+    // 12
   }
 
   //STEP 8
   function test_subtract_1() {
     var sub1 = 5;
-    var sub2 = 4;
-    // why function subtract diff color and doesnt
-    //subtract doesn't change to blue when function is
-    //is not present.
-    var test_subtract_1 = subtract(5, 4);
-    return test_subtract_1;
-    console.log(test_subtract_1);
+    var sub2 = 4; 
+    return subtract(5, 4);
+    // 1
   }
 
   //STEP 9
-  function test_subtract_2 () {
+  function test_subtract_2() {
     var ts1 = 5;
     var ts2 = 4;
     var ts3 = 3;
-    // why function subtract diff color and doesnt
-    //subtract doesn't change to blue when function is
-    //is not present.
     var ts4 = subtract(5,4);
     return ts3 - ts4;
-    console.log(ts3 - ts4);
+    // 2
   }
 
   //STEP 10-1
-  //why do we have to create variable for it to work
-  //shouldnt it just calc when call function with add(1,2)?
-  //could be complex when more variables involved
   var add1 = add(1, 2);
   console.log(add1);
   
@@ -83,37 +66,38 @@
   console.log(add22);
 
   //STEP 10-3
-  var add31 = 
+  var add31 = add(1, 2);
+  var add32 = add(3, 4);
+  var add33 = add(add31, add32);
   add(add(1, 2), add(3, 4));
-  console.log(add);
+  console.log(add33);
 
   //STEP 10-4
-  subtract(10, 9);
-  console.log(subtract);
+  var sub = subtract(10, 9);
+  console.log(sub);
 
   //STEP 10-5
-  subtract(10, subtract(19, 10));
-  console.log(subtract);
+  var sub5 = subtract(10, subtract(19, 10));
+  console.log(sub5);
 
   //STEP 10-6
-  subtract(subtract(20, 10), subtract(19, 10));
-  console.log(subtract);
+  var sub6 = subtract(subtract(20, 10), subtract(19, 10));
+  console.log(sub6);
 
   //STEP 10-7
-  multiply(2, 3);
-  console.log(multiply);
+  var mul = multiply(2, 3);
+  console.log(mul);
 
   //STEP 10-8
-  multiply(2, multiply(1, 3));
-  console.log(multiply);
+  var mul8 = multiply(2, multiply(1, 3));
+  console.log(mul8);
 
   //STEP 10-9
-  multiply(multiply(1, 2), multiply(3, 4));
-  console.log(multiply);
+  var mul9 = multiply(multiply(1, 2), multiply(3, 4));
+  console.log(mul9);
 
   //STEP 10-10
-  multiply(multiply(add(1, 3), subtract(8,4)), multiply(add(test_add_1, test_subtract_1), subtract(test_add_2, test_subtract_2)));
+  var mul10 = multiply(multiply(add(1, 2), subtract(8,4) ), multiply(add(test_add_1(), test_subtract_1() ), subtract(test_add_2(), test_subtract_2() )));
+  console.log(mul10);
 
-
-  console.log('test');
 }());
